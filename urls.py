ad988aa5cca4654d1fd52c3705e1f827b76358d2
koome <118932926+koome00@users.noreply.github.com>
2024-12-14 16:50:20 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,24 @@
 
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger setup
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ALX Travel App API",
+      default_version='v1',
+      description="API documentation for the ALX Travel App",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@alxtravelapp.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
+    path('api/', include('listings.urls')),  # Include your app URLs
+]
